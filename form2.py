d2a9700a91c53a4e850df4b33d142cc024ac330b
c00cjz00 <summerhill001@gmail.com>
2021-01-22 16:28:02 +0800

    init

--- form2.py
+++ form2.py
@@ -7,11 +7,14 @@ import shutil
 # 載入fastapi, 功能api框架
 from fastapi import FastAPI, File, UploadFile, Request, Form
 
+# 載入外掛資料夾
+from fastapi.staticfiles import StaticFiles
+
 # 載入Jinja2Templates, 網頁框架
 from fastapi.templating import Jinja2Templates
 
 # 載入starlette, 功能 Restful api
-from starlette.responses import RedirectResponse, StreamingResponse
+from starlette.responses import RedirectResponse
 
 # 載入  application/components/prediction/serve_model.py 之 Def:  predict, read_imagefile
 from serve_model import predict, read_imagefile
@@ -23,18 +26,22 @@ app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>
 # 定義 app FastAPI()
 app = FastAPI(title='Tensorflow FastAPI Starter Pack', description=app_desc)
 
+# 掛載資料夾
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 # 網頁範本
 templates = Jinja2Templates(directory='webTemplates/')
 
 # 家目錄 /
 @app.get("/", include_in_schema=False)
 async def index(request: Request):
-    # 開啟網頁templates/form2.html
+    # 開啟網頁templates/form3.html
     return templates.TemplateResponse('form2.html', context={'request': request})
 
 # 目錄 /predict/image, POST
 # 檔案上傳函式, 採用 UploadFile
-@app.post("/a")
+@app.post("/")
 async def predict_api(request: Request, email: str = Form(...), usr: str = Form(...), age: int = Form(...), file: UploadFile = File(...)):
     # 帶入參數
     email = email
@@ -53,23 +60,14 @@ async def predict_api(request: Request, email: str = Form(...), usr: str = Form(
         #    shutil.copyfileobj(file.file, buffer)
         # 圖片讀取, 啟動等待作業
         image = read_imagefile(await file.read())
-        image.save("tmp/1.png")
+        image.save("static/2.png")
         # 圖片預測函式
         predictionMessage = predict(image)
-        print("123allen__________________________<br><br>11")
     return templates.TemplateResponse('form2.html', context={'request': request, 'email': email, 'usr': usr, 'age': age, 'filename': filename, 'prediction': predictionMessage})
 
-
-@app.post("/")
-async def send(request: Request, file: UploadFile = File(...)):
-    image = read_imagefile(await file.read())
-    #img =                                        # Do something here to create an image
-    #img.save(image, format='JPEG', quality=85)   # Save image to BytesIO
-    #image.seek(0)                                # Return cursor to starting point
-    return StreamingResponse(image, media_type="image/png")
     
 # Python 直接執行 python main.py
 # 或  uvicorn main:app  --host 0.0.0.0 --port 9999
 if __name__ == "__main__":
     #uvicorn.run(app, debug=True)
-    uvicorn.run(app, port=9998, host='0.0.0.0')
+    uvicorn.run(app, port=9999, host='0.0.0.0')
